21ea700d6ca5791ef743f1a5f500aafa09be1616
Jacopo Esposito <jacopoesposito@MacStuddiJacopo.station>
2023-04-14 21:17:55 +0200

    Config .env variables and port setup for deployment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: __dirname + '/../.env.development' });
 
 import express, { json } from 'express';
 import cors from 'cors';
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app';
 
-app.listen({ port: 4000 }, () => {
+app.listen({ port: process.env.PORT || 4000 }, () => {
 	console.log('🚀 Server ready at http://localhost:4000');
 });

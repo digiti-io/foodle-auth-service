0a6408c93e93544cd4762341cfb3641adb198c23
Jacopo Esposito <jacopo.esposito@digiti.io>
2023-05-08 14:37:51 +0200

    Add sign-in route to auth service

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app';
 
 app.listen({ port: process.env.PORT || 4000 }, () => {
-	console.log('🚀 Server ready at http://localhost:4000');
+	console.log(`🚀 Server ready, up and running`);
 });
--- src/utils/middlewares/error-handler.ts
+++ src/utils/middlewares/error-handler.ts
@@ -1,20 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import { AuthServiceError } from '../../models/auth-service-errors/auth-service-error';
+import {
+	AuthServiceDataPayload,
+	AuthServiceMetaPayload,
+	AuthServiceResponsePayload,
+} from '../../models/auth-service-payload';
 
-export const errorHandler = (
-	err: Error,
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof AuthServiceError) {
 		const { errors, message, status } = err;
 
-		return res.status(status).send({
-			success: false,
-			message,
-			errors,
-			data: {},
-		});
+		const meta = new AuthServiceMetaPayload(message, false, errors);
+		const data = new AuthServiceDataPayload(null);
+
+		const response = new AuthServiceResponsePayload(meta, data);
+
+		return res.status(status).send(response);
 	}
 };
